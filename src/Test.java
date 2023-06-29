a850cb5344d75be7ac3497d121cf06da1ac01d1c
Reishandy <isthisruxury@gmail.com>
2023-06-30 01:52:03 +0700

    init 2

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+// Author: Muhammad Akbar Reishandy
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, UAS!!!");
+        System.out.println("And FFFF this stuff!!!");
+    }
+}
