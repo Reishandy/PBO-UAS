5c3a7ff4dc3a679224901cf7ccb2a90d357e006b
Reishandy <isthisruxury@gmail.com>
2023-06-30 03:42:40 +0700

    logic bookshelf

---
+++ src/logic/Bookshelf.java
@@ -0,0 +1,41 @@
+// Author: Muhammad Akbar Reishandy
+package logic;
+
+import java.util.ArrayList;
+
+public class Bookshelf {
+    private ArrayList<Book> books;
+
+    public Bookshelf() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void addBook(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public Book search(String title) {
+        for (Book book: books)
+            if (book.getTitle() != null && book.getTitle().contains(title)) return book;
+        return null;
+    }
+
+    public Book search(int id) {
+        for (Book book: books) {
+            if (book.getId() == id) return book;
+        }
+        return null;
+    }
+
+    public String[] listTitle() {
+        String[] list = new String[this.books.size()];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = this.books.get(i).getTitle();
+        }
+        return list;
+    }
+}
